1862d769437fd2139654e0c6204cbd49e1c438dc
diegomsc <diego.delacruz@mscdirect.com>
2024-05-31 09:12:18 -0600

    Checkbox component plugin version 1.0

--- components/msc-checkbox.js
+++ components/msc-checkbox.js
@@ -1,12 +1,14 @@
 module.exports = function ({ addComponents, theme }) {
   const checkbox = {
+    ".msc-checkbox-container": {
+      "@apply flex items-center gap-3": {},
+    },
     ".msc-checkbox": {
-      "@apply py-2 px-5 rounded-full font-bold text-base text-white cursor-pointer":
-        {},
-      backgroundColor: theme("colors.primary.blue"),
-      "&:hover": {
-        backgroundColor: "#186DF5",
-      },
+      "@apply w-5 h-5 rounded accent-black": {},
+      borderColor: theme("colors.monochromes.grey_light")
+    },
+    ".msc-checkbox-label": {
+      "@apply relative inline-flex items-center gap-3 cursor-pointer mb-0.5": {},
     },
   };
   addComponents(checkbox);
