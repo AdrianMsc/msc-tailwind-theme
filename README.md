4a30381c170b13bfbc7645e29eabbf522efe4e8c
AdrianLona <adrian.lona@mscsdirect.com>
2024-06-14 09:36:01 -0600

    fix:msc-buttons

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-`npx tailwindcss -i ./src/input.css -o ./src/output.css --watch`
+# FUEL MSC DESIGN SYSTEM
 
-# Website
+The **FUEL MSC DESIGN SYSTEM** is a design system created with the aim of centralizing and documenting MSC's web components. This system allows users and developers to interact efficiently with the components, facilitating their understanding and use.
 
-[https://adrianmsc.github.io/msc-tailwind-theme/](https://adrianmsc.github.io/msc-tailwind-theme/)
+## Objective
+
+Our goal is to provide a scalable and efficient method to keep the user interface updated and of high quality. We believe that good design and good documentation are key to achieving an excellent user experience.
+
+## Components
+
+The components of the design system are documented in detail, including examples of use, available properties, and events. This allows developers to quickly understand how to use each component and what functionalities it offers.
+
+## Interaction
+
+Users and developers can interact with the components directly in the documentation, allowing them to experience firsthand the behavior and functionalities of each component.
+
+## Maintenance
+
+The design system is maintained and updated regularly to ensure that the components follow the latest design trends and meet accessibility standards.
+
+
--- components/msc-buttons.js
+++ components/msc-buttons.js
@@ -1,7 +1,8 @@
 module.exports = function ({ addComponents, theme }) {
   const buttons = {
     ".msc-btn": {
-      "@apply py-2 px-5 rounded-full font-bold text-base cursor-pointer": {},
+      "@apply py-2 px-5 h-10 rounded-full font-bold text-base cursor-pointer":
+        {},
     },
     ".msc-btn-sm": {
       "@apply h-7 text-sm py-1": {},
@@ -19,6 +20,10 @@ module.exports = function ({ addComponents, theme }) {
         "@apply outline-dashed outline-1 outline-offset-4 outline-[#424242]":
           {},
       },
+      "&:disabled": {
+        "@apply text-white cursor-not-allowed": {},
+        backgroundColor: theme("colors.monochromes.grey_light"),
+      },
     },
     ".msc-btn-blue-outline": {
       "@apply bg-transparent border": {},
@@ -34,6 +39,14 @@ module.exports = function ({ addComponents, theme }) {
         "@apply outline-dashed outline-1 outline-offset-4 outline-[#424242]":
           {},
       },
+      "&:disabled": {
+        "@apply bg-transparent cursor-not-allowed border": {},
+        color: theme("colors.monochromes.grey_light"),
+        borderColor: theme("colors.monochromes.grey_light"),
+      },
+      "& .msc-btn-dot1, .msc-btn-dot2, .msc-btn-dot3": {
+        backgroundColor: theme("colors.primary.blue"),
+      },
     },
     ".msc-btn-transparent": {
       "@apply bg-transparent": {},
@@ -48,62 +61,30 @@ module.exports = function ({ addComponents, theme }) {
         "@apply outline-dashed outline-1 outline-offset-4 outline-[#424242]":
           {},
       },
-    },
-    ".msc-btn-solid-disabled": {
-      "@apply text-white cursor-not-allowed": {},
-      backgroundColor: theme("colors.monochromes.grey_light"),
-    },
-    ".msc-btn-outline-disabled": {
-      "@apply bg-transparent cursor-not-allowed border": {},
-      color: theme("colors.monochromes.grey_light"),
-      borderColor: theme("colors.monochromes.grey_light"),
-    },
-    ".msc-btn-transparent-disabled": {
-      "@apply bg-transparent cursor-not-allowed": {},
-      color: theme("colors.monochromes.grey_light"),
+      "&:disabled": {
+        "@apply bg-transparent cursor-not-allowed": {},
+        color: theme("colors.monochromes.grey_light"),
+      },
+      "& .msc-btn-dot1, .msc-btn-dot2, .msc-btn-dot3": {
+        backgroundColor: theme("colors.primary.blue"),
+      },
     },
     ".msc-btn-icon": {
       "@apply flex items-center ml-2": {},
     },
     ".msc-btn-dots-container": {
-      "@apply w-10 h-6 flex place-content-between items-center": {},
-    },
-    ".msc-btn-dots-container-sm": {
-      "@apply w-10 h-5 flex place-content-between items-center": {},
+      "@apply w-10 h-auto flex place-content-between items-center": {},
     },
     ".msc-btn-dot1": {
-      "@apply relative top-0 w-2 h-2 bg-white rounded-full animate-dotFlashing1":
-        {
-          left: "3.75rem",
-        },
+      "@apply relative w-2 h-2 bg-white rounded-full animate-dotFlashing1": {},
     },
     ".msc-btn-dot2": {
       "@apply relative w-2 h-2 bg-white rounded-full animate-dotFlashing2 opacity-50":
         {},
     },
     ".msc-btn-dot3": {
-      "@apply relative top-0 w-2 h-2 bg-white rounded-full animate-dotFlashing3 opacity-30":
-        {
-          left: "3.75rem",
-        },
-    },
-    ".msc-btn-dot1-blue": {
-      "@apply relative top-0 w-2 h-2 rounded-full animate-dotFlashing1": {
-        left: "3.75rem",
-      },
-      backgroundColor: theme("colors.primary.blue"),
-    },
-    ".msc-btn-dot2-blue": {
-      "@apply relative w-2 h-2 rounded-full animate-dotFlashing2 opacity-50":
+      "@apply relative w-2 h-2 bg-white rounded-full animate-dotFlashing3 opacity-30":
         {},
-      backgroundColor: theme("colors.primary.blue"),
-    },
-    ".msc-btn-dot3-blue": {
-      "@apply relative top-0 w-2 h-2 rounded-full animate-dotFlashing3 opacity-30":
-        {
-          left: "3.75rem",
-        },
-      backgroundColor: theme("colors.primary.blue"),
     },
   };
   addComponents(buttons);
