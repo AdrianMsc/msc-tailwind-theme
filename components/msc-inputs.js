b6069dbf41f106c4191853425c9bdac082b0d111
AdrianLona <adrian.lona@mscsdirect.com>
2024-06-18 12:30:50 -0600

    BB Sync inputs

--- components/msc-inputs.js
+++ components/msc-inputs.js
@@ -1,46 +1,27 @@
 module.exports = function ({ addComponents, theme }) {
   const input = {
     ".msc-input": {
-      "@apply w-full bg-white border border-monochromes-grey_light hover:border-monochromes-main px-4 py-3.5 rounded text-sm placeholder-transparent focus:border-monochromes-grey_light focus:outline-dashed focus:outline-1 focus:outline-offset-4 focus:outline-black h-10":
-        {},
+      "@apply bg-white w-full h-10 px-4 py-3.5 text-sm rounded border !border-monochromes-grey_light placeholder-transparent disabled:bg-off_white disabled:!border-[#e5e7eb] disabled:cursor-not-allowed disabled:text-monochromes-grey_light hover:disabled:!border-[#e5e7eb] hover:enabled:!border-monochromes-main focus:border-monochromes-grey_light focus:outline-dashed focus:outline-1 focus:outline-offset-4 focus:outline-black invalid:!border-error-red invalid:!text-error-red hover:invalid:!border-error-red focus:invalid:!border-error-red":{},
     },
     ".msc-input-label": {
-      "@apply bg-white absolute left-3.5 -top-[0.6rem] pt-[1px] px-[0.2rem] text-xs transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:text-monochromes-grey_light peer-placeholder-shown:top-2 peer-focus:-top-[0.6rem] peer-focus:text-monochromes-main peer-focus:text-xs":
+      "@apply bg-white absolute left-3.5 -top-[0.6rem] pt-[1px] px-[0.2rem] text-xs transition-all cursor-text peer-placeholder-shown:text-sm peer-placeholder-shown:text-monochromes-grey_light peer-placeholder-shown:top-2 peer-focus:-top-[0.6rem] peer-focus:text-[#212121] peer-focus:text-xs peer-focus:cursor-default peer-invalid:text-error-red peer-disabled:bg-off_white":
         {},
     },
     ".msc-input-wrapper": {
-      "@apply  relative w-full flex-col": {},
-    },
-    ".msc-input-disabled": {
-      "@apply w-full border text-monochromes-grey_light px-4 py-3.5 rounded text-sm focus:outline-none focus:border-monochromes-grey_light  cursor-not-allowed h-10":
-        {},
-    },
-    ".msc-label-disabled": {
-      "@apply text-monochromes-grey_xlight !important": {},
+      "@apply  relative w-full": {},
     },
     ".msc-input-unit": {
-      "@apply  text-center border border-monochromes-grey_light hover:border-monochromes-main  md:pr-0 py-3.5 rounded text-sm focus:border-monochromes-main focus:outline-dashed focus:outline-1 focus:outline-offset-4 focus:outline-black h-10 no-arrows":
+      "@apply  text-center max-w-[68px] border border-monochromes-grey_light hover:border-monochromes-main pl-4 pr-4 py-3.5 rounded text-sm focus:border-monochromes-grey_light focus:outline-dashed focus:outline-1 focus:outline-offset-4 focus:outline-black h-10 disabled:bg-off_white disabled:!border-[#e5e7eb] disabled:cursor-not-allowed hover:disabled:!border-[#e5e7eb] invalid:!border-error-red invalid:!text-error-red hover:invalid:!border-error-red focus:invalid:!border-error-red":
         {},
     },
     ".msc-input-unit-label": {
-      "@apply text-monochromes-main text-sm mb-1 text-xs text-center": {},
-    },
-    ".msc-input-unit-disabled": {
-      "@apply w-full border text-center text-monochromes-grey_light py-3.5 rounded text-sm focus:outline-none focus:border-monochromes-grey_light  cursor-not-allowed h-10 no-arrows":
-        {},
+      "@apply text-monochromes-main text-sm mb-1 text-xs text-center peer-disabled:text-monochromes-grey_xlight": {},
     },
     ".msc-input-unit-wrapper": {
-      "@apply relative flex flex-col w-[68px]": {},
+      "@apply relative flex flex-col max-w-[68px] w-[68px]": {},
     },
-    ".input-error": {
-      "@apply border-error-red hover:border-error-red text-error-red focus:border-error-red":
-        {},
-    },
-    ".label-error": {
-      "@apply peer-placeholder-shown:text-error-red peer-focus:text-black": {},
-    },
-    ".error-message": {
-      "@apply  text-error-red font-semibold !important": {},
+    ".msc-error-message": {
+      "@apply  text-error-red font-semibold mt-1 hidden peer-invalid:block !important": {},
     },
   };
 
