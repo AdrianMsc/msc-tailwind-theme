c87daff465bc0591af031f6173d9606f1f321391
diegomsc <diego.delacruz@mscdirect.com>
2024-06-26 14:27:23 -0600

    FEAT: Alphabetic Pager Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,6 @@ module.exports = {
     require("./components/msc-dividers"),
     require("./components/msc-spinner"),
     require("./components/msc-link"),
+    require("./components/msc-pager"),
   ],
 };
