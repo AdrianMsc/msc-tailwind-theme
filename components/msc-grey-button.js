09a03310f20e2232835dc16273599d140eeace49
diegomsc <diego.delacruz@mscdirect.com>
2024-07-12 12:31:42 -0600

    FIX: Colors changes on Grey button

--- components/msc-grey-button.js
+++ components/msc-grey-button.js
@@ -1,8 +1,14 @@
 module.exports = function ({ addComponents, theme }) {
   const greyButton = {
     ".msc-grey-button": {
-      "@apply flex gap-[6px] items-center py-[13px] px-[10px] text-sm font-normal text-monochromes-grey_light hover:text-monochromes-grey_dark":
+      "@apply flex gap-[6px] items-center py-[13px] px-[10px] text-sm font-normal text-monochromes-grey leading-[14px]":
         {},
+      "&:hover": {
+        "@apply text-monochromes-main": {},
+      },
+      "&:disabled": {
+        "@apply text-monochromes-grey_light cursor-not-allowed": {},
+      },
     },
   };
   addComponents(greyButton);
