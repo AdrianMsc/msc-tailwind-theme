b7f9675a6bf0f8cbb5cf4d48db1e2fee84d5dfb2
AdrianLona <adrian.lona@mscsdirect.com>
2024-07-17 07:51:04 -0600

    feat:msc tabs update

--- components/msc-tabs.js
+++ components/msc-tabs.js
@@ -1,7 +1,7 @@
 module.exports = function ({ addComponents, theme }) {
   const tabs = {
     ".msc-tabs": {
-      "@apply flex w-full": {},
+      "@apply flex w-full h-[50px]": {},
     },
     ".msc-tabs li": {
       "@apply text-[16px] p-3 grow text-center cursor-pointer transition-all after:transition-all after:duration-500  relative after:w-0 text-monochromes-grey border-gray-700 border-opacity-[16%] border-b hover:font-bold hover:text-black border-b hover:border-black font-bold":
@@ -11,10 +11,10 @@ module.exports = function ({ addComponents, theme }) {
       "@apply text-black p-3 grow text-center font-bold after:bg-primary-blue  after:h-[2px] after:w-full after:absolute after:left-0 after:bottom-0 hover:border-transparent":
         {},
     },
-    ".msc-tabs .middle": {
-      "@apply relative before:absolute before:left-0 before:bottom-0 before:h-full before:w-[1px] before:bg-slate-500 before:opacity-[16%]  after:absolute after:right-0 after:bottom-0 after:h-full after:w-[1px] after:bg-slate-500 after:opacity-[16%]":
-        {},
-    },
+    // ".msc-tabs .middle": {
+    //   "@apply border-l border-gray-700 border-r border-gray-700 border-opacity-[16%] hover:border-opacity-[16%]":
+    //     {},
+    // },
   };
   addComponents(tabs);
 };
