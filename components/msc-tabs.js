0328036c5071dead7b8bf48867f89ef1e8b09f04
AdrianLona <adrian.lona@mscsdirect.com>
2024-07-22 13:40:16 -0600

    feat: msc-flex tabs

--- components/msc-tabs.js
+++ components/msc-tabs.js
@@ -1,20 +1,23 @@
 module.exports = function ({ addComponents, theme }) {
   const tabs = {
     ".msc-tabs": {
-      "@apply flex w-full h-[50px]": {},
+      "@apply grid auto-cols-min w-full overflow-x-auto md:overflow-x-hidden":
+        {},
     },
     ".msc-tabs li": {
-      "@apply text-[16px] p-3 grow text-center cursor-pointer transition-all after:transition-all after:duration-500  relative after:w-0 text-monochromes-grey border-gray-700 border-opacity-[16%] border-b hover:font-bold hover:text-black border-b hover:border-black font-bold":
+      "@apply text-[16px] grow text-center cursor-pointer transition-all after:transition-all after:duration-500  relative after:w-0 text-monochromes-grey border-gray-700 border-opacity-[16%] border-b hover:font-bold hover:text-black border-b hover:border-black font-bold":
         {},
+    },
+    ".msc-tabs li a": {
+      "@apply p-3 border-r border-gray-200 w-full block text-nowrap": {},
+    },
+    ".msc-tabs :last-child a": {
+      "@apply !border-r-0": {},
     },
     ".msc-tabs .active": {
-      "@apply text-black p-3 grow text-center font-bold after:bg-primary-blue  after:h-[2px] after:w-full after:absolute after:left-0 after:bottom-0 hover:border-transparent":
+      "@apply text-black grow text-center font-bold after:!bg-primary-blue  after:h-[2px] after:w-full after:absolute after:left-0 after:bottom-0 hover:border-transparent":
         {},
     },
-    // ".msc-tabs .middle": {
-    //   "@apply border-l border-gray-700 border-r border-gray-700 border-opacity-[16%] hover:border-opacity-[16%]":
-    //     {},
-    // },
   };
   addComponents(tabs);
 };
