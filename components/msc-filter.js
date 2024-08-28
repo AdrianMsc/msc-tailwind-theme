1ed0f6b9f74fb7abe10782536df0ad77371999ee
diegomsc <diego.delacruz@mscdirect.com>
2024-08-28 14:46:07 -0600

    FIX: styles added to active

--- components/msc-filter.js
+++ components/msc-filter.js
@@ -7,9 +7,6 @@ module.exports = function ({ addComponents }) {
         borderColor: "#000000",
       },
     },
-    // ".msc-top-filter p": {
-    //   "@apply align-middle text-nowrap w-full text-clip": {},
-    // },
     ".msc-top-filter p": {
       "@apply text-nowrap truncate align-middle": {},
     },
@@ -17,15 +14,18 @@ module.exports = function ({ addComponents }) {
       "@apply bg-white": {},
     },
     ".msc-top-filter-active": {
-      "@apply border border-monochromes-grey_light rounded bg-white h-8 w-[216px] py-1.5 px-3 items-center text-monochromes-grey flex flex-row hover:border-black text-black":
+      "@apply border border-monochromes-grey_light rounded bg-white h-8 py-1.5 px-3 items-center text-monochromes-grey hover:border-black text-black flex w-full overflow-hidden justify-between":
         {},
-
       "&:hover": {
         borderColor: "#000000",
       },
     },
+    ".msc-top-filter-active p": {
+      "@apply text-nowrap truncate align-middle": {},
+    },
     ".msc-top-filter-badge": {
-      "@apply text-white rounded-full bg-black size-4 text-xs items-center": {},
+      "@apply text-white rounded-full bg-black size-4 text-xs items-center min-w-4":
+        {},
     },
     ".msc-top-filters-section": {
       "@apply flex flex-col gap-2 w-full bg-[#F2F2F2] p-3 border border-primary-blue rounded overflow-auto":
