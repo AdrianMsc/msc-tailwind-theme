b347d4fd5b6e2399c79f2dd9bb44edbbee650568
diegomsc <diego.delacruz@mscdirect.com>
2024-11-08 11:12:26 -0600

    WIP: Table Modal

--- utilities/index.js
+++ utilities/index.js
@@ -37,6 +37,14 @@ const sideNav = `
         </li>
       </ul>
     </li>
+    <li>
+          <a
+            href="table.html"
+            class="flex items-center px-2 text-gray-900 rounded-lg pl-5 hover:bg-gray-100 group"
+          >
+            <span class="flex-1 ms-3 whitespace-nowrap">Table Modal</span>
+          </a>
+        </li>
     <!-- FOUNDATIONS  -->
     <li>
       <button
