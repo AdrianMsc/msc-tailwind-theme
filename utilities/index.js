d39e46e18985b18b4f1b000232d514f475a41b39
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-01-24 08:25:45 -0600

    404

--- utilities/index.js
+++ utilities/index.js
@@ -254,7 +254,16 @@ const sideNav = `
             <span class="flex-1 ms-3 whitespace-nowrap">Alerts</span>
           </a>
         </li>
+         <li>
+          <a
+            href="404.html"
+            class="flex items-center px-2 text-gray-900 rounded-lg pl-5 hover:bg-gray-100 group"
+          >
+            <span class="flex-1 ms-3 whitespace-nowrap">Error 404</span>
+          </a>
+        </li>
       </ul>
+      
     </li>
 
     <!-- LAYOUT  -->
